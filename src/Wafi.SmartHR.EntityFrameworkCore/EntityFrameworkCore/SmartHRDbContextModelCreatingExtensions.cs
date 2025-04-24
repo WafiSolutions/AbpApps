f7236d39273a8e5e7a6cf612e6e7f14bab4e9d0f
mohammadEunus <100eunus@gmail.com>
2025-04-24 19:42:49 +0600

    temp

---
+++ src/Wafi.SmartHR.EntityFrameworkCore/EntityFrameworkCore/SmartHRDbContextModelCreatingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+using Wafi.SmartHR.Employees;
+using Wafi.SmartHR.LeaveRecords;
+
+namespace Wafi.SmartHR.EntityFrameworkCore;
+
+public static class SmartHRDbContextModelCreatingExtensions
+{
+    public static void ConfigureSmartHR(
+        this ModelBuilder builder)
+    {
+        Check.NotNull(builder, nameof(builder));
+
+
+        builder.Entity<Employee>(b =>
+        {
+            b.ToTable(SmartHRDbProperties.DbTablePrefix + "Employees", SmartHRDbProperties.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<LeaveRecord>(b =>
+        {
+            b.ToTable(SmartHRDbProperties.DbTablePrefix + "LeaveRecords", SmartHRDbProperties.DbSchema);
+            b.ConfigureByConvention();
+        });
+
+    }
+}
