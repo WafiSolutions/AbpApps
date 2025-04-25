a2ffb054319005fd71fa5ce7c06293efb62c71b0
mohammadEunus <100eunus@gmail.com>
2025-04-25 23:41:09 +0600

    creation time added

--- src/Wafi.SmartHR.Web/Pages/Chats/Index.cshtml.cs
+++ src/Wafi.SmartHR.Web/Pages/Chats/Index.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Volo.Abp.Timing;
 
 namespace Wafi.SmartHR.Web.Pages.Chats;
 
@@ -41,7 +43,7 @@ public class IndexModel(HttpClient httpClient) : PageModel
             }
             else
             {
-                Conversation.Add(new Message { Sender = "AI", Content = "Error getting response from AI." });
+                Conversation.Add(new Message { Sender = "AI", Content = "Error getting response from AI.", CreationTime = DateTime.Now });
             }
         }
 
@@ -68,6 +70,7 @@ public class IndexModel(HttpClient httpClient) : PageModel
     {
         public string Sender { get; set; }
         public string Content { get; set; }
+        public DateTime CreationTime { get; set; }
     }
 
     public class AIResponse
