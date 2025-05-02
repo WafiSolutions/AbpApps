836f9cbcf856f9c5e677fa9af5d7dad66fd5a9e2
mohammad eunus <100eunus@gmail.com>
2025-05-02 11:37:55 +0600

    remvoed

--- test/Wafi.SmartHR.Application.Tests/SmartHRApplicationTestBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Modularity;
-
-namespace Wafi.SmartHR;
-
-public abstract class SmartHRApplicationTestBase<TStartupModule> : SmartHRTestBase<TStartupModule>
-    where TStartupModule : IAbpModule
-{
-
-}
