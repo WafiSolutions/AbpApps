1b9fcb590c6918a1e71678ede0b1c3191d15e28f
mohammad eunus <100eunus@gmail.com>
2025-05-02 22:03:46 +0600

    Update AiAppServiceTests.cs

--- test/Wafi.Abp.OpenAISemanticKernel.Tests/Services/AiAppServiceTests.cs
+++ test/Wafi.Abp.OpenAISemanticKernel.Tests/Services/AiAppServiceTests.cs
@@ -24,20 +24,19 @@ public class AiAppServiceTests : OpenAISemanticKernelTestBase
     }
 
     /// <summary>
-    /// Verifies that user messages are correctly added to the chat history.
+    /// Verifies that ai is responding.
     /// </summary>
     [Fact]
-    public async Task Should_Add_User_Message_To_History()
+    public async Task Should_Return_Answer_When_Asked()
     {
         // Arrange
         var message = "Hello, I need help";
         var request = new AskRequestDto
         {
             Question = message,
-            History = new List<Message>
-            {
-                new Message { UserType = UserType.User, Content = message }
-            }
+            History = [
+                new() { UserType = UserType.User, Content = message }
+            ]
         };
 
         // Act
@@ -58,10 +57,9 @@ public class AiAppServiceTests : OpenAISemanticKernelTestBase
         var firstRequest = new AskRequestDto
         {
             Question = firstQuestion,
-            History = new List<Message>
-            {
-                new Message { UserType = UserType.User, Content = firstQuestion }
-            }
+            History = [
+                new() { UserType = UserType.User, Content = firstQuestion }
+            ]
         };
 
         // First interaction
@@ -72,12 +70,11 @@ public class AiAppServiceTests : OpenAISemanticKernelTestBase
         var secondRequest = new AskRequestDto
         {
             Question = secondQuestion,
-            History = new List<Message>
-            {
-                new Message { UserType = UserType.User, Content = firstQuestion },
-                new Message { UserType = UserType.Ai, Content = firstResponse.Answer },
-                new Message { UserType = UserType.User, Content = secondQuestion }
-            }
+            History = [
+                new() { UserType = UserType.User, Content = firstQuestion },
+                new() { UserType = UserType.Ai, Content = firstResponse.Answer },
+                new() { UserType = UserType.User, Content = secondQuestion }
+            ]
         };
 
         // Act
