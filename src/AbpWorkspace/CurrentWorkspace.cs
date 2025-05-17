9bc4661f1fd01b05d945b0fabb258615826a085b
mohammad eunus <100eunus@gmail.com>
2025-05-17 17:06:43 +0600

    Update CurrentWorkspace.cs

--- src/AbpWorkspace/CurrentWorkspace.cs
+++ src/AbpWorkspace/CurrentWorkspace.cs
@@ -5,86 +5,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Wafi.Abp.Workspace
+namespace Wafi.Abp.Workspace;
+
+public class CurrentWorkspace : ICurrentWorkspace
 {
-    public class CurrentWorkspace : ICurrentWorkspace
+    private readonly AsyncLocal<WorkspaceCacheItem> _currentWorkspaceCacheItem = new AsyncLocal<WorkspaceCacheItem>();
+    private readonly ConcurrentDictionary<string, WorkspaceCacheItem> _workspaceConfigurations = new ConcurrentDictionary<string, WorkspaceCacheItem>();
+    /// <summary>
+    /// Gets current workspace's Id.
+    /// </summary>
+    public virtual Guid? Id => _currentWorkspaceCacheItem.Value?.WorkspaceId;
+    /// <summary>
+    /// Gets current workspace's name.
+    /// </summary>
+    public virtual string Name => _currentWorkspaceCacheItem.Value?.Name;
+    /// <summary>
+    /// Gets a value indicates that current workspace is available.
+    /// </summary>
+    public virtual bool IsAvailable => Id.HasValue;
+    /// <summary>
+    /// Changes current workspace Id.
+    /// </summary>
+    /// <param name="id">Workspace Id</param>
+    /// <returns>A disposable object to restore workspace Id when disposed.</returns>
+    public virtual IDisposable Change(Guid? id)
     {
-        private readonly AsyncLocal<WorkspaceCacheItem> _currentWorkspaceCacheItem = new AsyncLocal<WorkspaceCacheItem>();
-        private readonly ConcurrentDictionary<string, WorkspaceCacheItem> _workspaceConfigurations = new ConcurrentDictionary<string, WorkspaceCacheItem>();
-        /// <summary>
-        /// Gets current workspace's Id.
-        /// </summary>
-        public virtual Guid? Id => _currentWorkspaceCacheItem.Value?.WorkspaceId;
-        /// <summary>
-        /// Gets current workspace's name.
-        /// </summary>
-        public virtual string Name => _currentWorkspaceCacheItem.Value?.Name;
-        /// <summary>
-        /// Gets a value indicates that current workspace is available.
-        /// </summary>
-        public virtual bool IsAvailable => Id.HasValue;
-        /// <summary>
-        /// Changes current workspace Id.
-        /// </summary>
-        /// <param name="id">Workspace Id</param>
-        /// <returns>A disposable object to restore workspace Id when disposed.</returns>
-        public virtual IDisposable Change(Guid? id)
+        return Change(id, null);
+    }
+    /// <summary>
+    /// Changes current workspace Id and Name.
+    /// </summary>
+    /// <param name="id">Workspace Id</param>
+    /// <param name="name">Workspace Name</param>
+    /// <returns>A disposable object to restore workspace values when disposed.</returns>
+    public virtual IDisposable Change(Guid? id, string name)
+    {
+        var workspaceCacheItem = _currentWorkspaceCacheItem.Value;
+        var previousWorkspaceId = workspaceCacheItem?.WorkspaceId;
+        var previousWorkspaceName = workspaceCacheItem?.Name;
+        if (id == previousWorkspaceId && name == previousWorkspaceName)
         {
-            return Change(id, null);
+            return NullWorkspaceRestore.Instance;
         }
-        /// <summary>
-        /// Changes current workspace Id and Name.
-        /// </summary>
-        /// <param name="id">Workspace Id</param>
-        /// <param name="name">Workspace Name</param>
-        /// <returns>A disposable object to restore workspace values when disposed.</returns>
-        public virtual IDisposable Change(Guid? id, string name)
+        _currentWorkspaceCacheItem.Value = new WorkspaceCacheItem(id, name);
+        return new WorkspaceRestore(this, previousWorkspaceId, previousWorkspaceName);
+    }
+    private class WorkspaceCacheItem
+    {
+        public Guid? WorkspaceId { get; }
+        public string Name { get; }
+        public WorkspaceCacheItem(Guid? workspaceId, string name = null)
         {
-            var workspaceCacheItem = _currentWorkspaceCacheItem.Value;
-            var previousWorkspaceId = workspaceCacheItem?.WorkspaceId;
-            var previousWorkspaceName = workspaceCacheItem?.Name;
-            if (id == previousWorkspaceId && name == previousWorkspaceName)
-            {
-                return NullWorkspaceRestore.Instance;
-            }
-            _currentWorkspaceCacheItem.Value = new WorkspaceCacheItem(id, name);
-            return new WorkspaceRestore(this, previousWorkspaceId, previousWorkspaceName);
+            WorkspaceId = workspaceId;
+            Name = name;
         }
-        private class WorkspaceCacheItem
+    }
+    private class WorkspaceRestore : IDisposable
+    {
+        private readonly CurrentWorkspace _currentWorkspace;
+        private readonly Guid? _workspaceId;
+        private readonly string _workspaceName;
+        public WorkspaceRestore(CurrentWorkspace currentWorkspace, Guid? workspaceId, string workspaceName = null)
         {
-            public Guid? WorkspaceId { get; }
-            public string Name { get; }
-            public WorkspaceCacheItem(Guid? workspaceId, string name = null)
-            {
-                WorkspaceId = workspaceId;
-                Name = name;
-            }
+            _currentWorkspace = currentWorkspace;
+            _workspaceId = workspaceId;
+            _workspaceName = workspaceName;
         }
-        private class WorkspaceRestore : IDisposable
+        public void Dispose()
         {
-            private readonly CurrentWorkspace _currentWorkspace;
-            private readonly Guid? _workspaceId;
-            private readonly string _workspaceName;
-            public WorkspaceRestore(CurrentWorkspace currentWorkspace, Guid? workspaceId, string workspaceName = null)
-            {
-                _currentWorkspace = currentWorkspace;
-                _workspaceId = workspaceId;
-                _workspaceName = workspaceName;
-            }
-            public void Dispose()
-            {
-                _currentWorkspace._currentWorkspaceCacheItem.Value = new WorkspaceCacheItem(_workspaceId, _workspaceName);
-            }
+            _currentWorkspace._currentWorkspaceCacheItem.Value = new WorkspaceCacheItem(_workspaceId, _workspaceName);
         }
-        private class NullWorkspaceRestore : IDisposable
+    }
+    private class NullWorkspaceRestore : IDisposable
+    {
+        public static readonly NullWorkspaceRestore Instance = new NullWorkspaceRestore();
+        private NullWorkspaceRestore()
         {
-            public static readonly NullWorkspaceRestore Instance = new NullWorkspaceRestore();
-            private NullWorkspaceRestore()
-            {
-            }
-            public void Dispose()
-            {
-            }
+        }
+        public void Dispose()
+        {
         }
     }
 }
