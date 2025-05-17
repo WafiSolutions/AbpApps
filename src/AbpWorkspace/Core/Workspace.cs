46cc3c70e3b3576cad37d3d89b2dce63bcf8451b
mohammad eunus <100eunus@gmail.com>
2025-05-17 17:28:45 +0600

    Update Workspace.cs

--- src/AbpWorkspace/Core/Workspace.cs
+++ src/AbpWorkspace/Core/Workspace.cs
@@ -1,8 +1,11 @@
 ﻿using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
 
 namespace Wafi.Abp.Workspaces.Core;
 
-public class Workspace : AuditedAggregateRoot<Guid>
+public class Workspace : AuditedAggregateRoot<Guid>, IMultiTenant
 {
+    public Guid? TenantId { get; set; }
     public string Name { get; set; }
+
 }
